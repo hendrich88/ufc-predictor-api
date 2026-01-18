0afa4b03af37c647f69f192ae49061320f67ed65
hendrich88 <hendrich88@gmail.com>
2026-01-18 19:41:11 +0100

    Create input.py

---
+++ input.py
@@ -0,0 +1,6 @@
+# Vstupní seznamy zápasníků
+event_fighters1 = ["Justin Gaethje","Kayla Harrison","Sean O'Malley","Waldo Cortes Acosta","Arnold Allen","Natalia Silva","Umar Nurmagomedov",
+                   "Ateba Gautier","Michael Johnson","Nikita Krylov","Alex Perez","Ricky Turcios"] 
+
+event_fighters2 = ["Paddy Pimblett","Amanda Nunes","Song Yadong","Derrick Lewis","Jean Silva","Rose Namajunas","Deiveson Figueiredo",
+                   "Andrey Pulyaev","Alexander Hernandez","Modestas Bukauskas","Charles Johnson","Cameron Smotherman"]
