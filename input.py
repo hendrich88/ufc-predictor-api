ab0b5a98922370f85d19b07aca996fffae93746b
hendrich88 <hendrich88@gmail.com>
2026-01-18 23:36:50 +0100

    Update input.py

--- input.py
+++ input.py
@@ -9,10 +9,10 @@ event_fighters2 = ["Paddy Pimblett","Song Yadong","Derrick Lewis","Jean Silva","
                    "Andrey Pulyaev","Alexander Hernandez","Modestas Bukauskas","Charles Johnson","Cameron Smotherman"]
 
 # Sázkové kurzy
-odds_fighters1 = [1.4, 1.47, 1.26, 2.79, 1.22, 1.06, 
+odds_fighters1 = [2.69, 1.47, 1.26, 2.79, 1.22, 1.06, 
                   1.1, 2.15, 2.17, 2.51, 2.41] 
 
-odds_fighters2 = [2.69, 2.48, 3.37, 1.37, 3.74, 7.33, 
+odds_fighters2 = [1.4, 2.48, 3.37, 1.37, 3.74, 7.33, 
                   5.75, 1.64, 1.62, 1.46, 1.49]
 
 hit = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
